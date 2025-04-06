2737fec55bda11918bc6f275eeac9127277f70a3
Michael Teixeira <meixeira@gmail.com>
2025-04-06 17:51:09 -0400

    Add tanstack Query provider and ChatContainer

---
+++ src/client/components/ChatContainer.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useRef } from 'react';
+
+const ChatContainer: React.FC = () => {
+  
+
+  return (
+    <div className="flex flex-col h-screen md:min-w-lg max-w-3xl mx-auto rounded-lg overflow-hidden shadow-md bg-white">
+    </div>
+  );
+};
+
+export default ChatContainer; 
