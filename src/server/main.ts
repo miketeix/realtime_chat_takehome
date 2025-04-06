637bb973e32bdefc8f05d2917c0219124b19ba4f
Michael Teixeira <meixeira@gmail.com>
2025-04-06 17:46:42 -0400

    Add express proxy middleware

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,7 +2,24 @@ import dotenv from "dotenv"
 dotenv.config()
 import express from "express";
 import ViteExpress from "vite-express";
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
 const app = express();
+
+const proxyMiddleware = createProxyMiddleware<Request, Response>({
+  target: 'https://api.openphone.com/v1/',
+  changeOrigin: true,
+  pathRewrite: {'^/openphone-api' : ''},
+  on: {
+    proxyReq: (proxyReq, req, res) => {
+      /* handle proxyReq */
+      proxyReq.setHeader('Authorization', process.env.OPENPHONE_API_KEY as string);
+    },
+  }
+});
+
+app.use('/openphone-api', proxyMiddleware);
+
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
